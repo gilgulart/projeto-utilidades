1b6dabda1050f832cd3714ecdb0d3d46a4d1d31b
gilberto <gilbertogulart70@gmail.com>
2026-05-04 12:52:43 -0300

    feat: exibindo números e operadores no display

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,16 +1,23 @@
-let currentValue = "0";
+let currentValue = 0;
 let beforeValue = null;
 let operator = null;
 let waitingSec = false;
 
+const display = document.querySelector(".result");
+
+function output(n) {
+    display.innerText = n;
+}
+
+
 function clickNumber(num) {
     if (waitingSec){
         currentValue = num;
-        waitingSec = false
-        console.log("valor atual " + currentValue)
+        waitingSec = false;
+        console.log("valor atual " + currentValue);
         
     } else {
-        currentValue = currentValue === "0"
+        currentValue = currentValue === 0
         ? num
         : currentValue + num;
         console.log("valor atual " + currentValue)
@@ -23,10 +30,15 @@ function clickOperator(op) {
     beforeValue = currentValue;
     operator = op; 
     waitingSec = true;
+    
+    return operator;
 }
 
+
 function displayClear() {
-    currentValue = "0"
+    currentValue = "";
+    display.innerText = currentValue;
+
 }
 
 
@@ -34,17 +46,20 @@ const buttons = document.querySelector(".buttons")
 buttons.addEventListener('click', (e) =>{
     const clicked = e.target;
     if (clicked.classList.contains("btn-number")) {
-        clickNumber(clicked.innerText)
-        console.log("número: " + clicked.innerText);
+        clickNumber(clicked.innerText);
+        output(currentValue);
+        // console.log("número: " + clicked.innerText);
         
     }
-    if (clicked.classList.contains("sum")) {
-        console.log("soma")
+    if (clicked.classList.contains("operator")) {
+        operator = clicked.innerText;
+        clickOperator(operator)
+        // console.log("operador" + operator);
     }
 
     if (clicked.classList.contains("btn-clear")) {
-        displayClear()
-        console.log("limpo " + currentValue);
+        displayClear();
+        // console.log("limpo " + currentValue);
         
     }
 
